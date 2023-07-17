1c37aa123dd416baf47eff8761629a29b6079fe5
Gabriel Ferraz <gabrielferraz97@outlook.com>
2023-07-17 18:41:45 -0300

    Aplicação menu interativo, javascript concluido

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Está funcionando corretamente!');
+const toggleMenu = document.getElementById('toggleMenu');
+const menuButton = document.getElementById('menuButton');
+
+
+menuButton.addEventListener('click', () => {
+    console.log('Funcionando');
+    toggleMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.toggleItem').forEach(n => n.addEventListener('click', () => {
+    console.log('Retornando');
+    toggleMenu.classList.remove('active');
+
+}))
