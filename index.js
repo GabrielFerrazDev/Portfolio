1bcd76f5275792cbc2b22f8cfc17b44d348408e1
Gabriel Ferraz <gabrielferraz97@outlook.com>
2023-07-18 13:21:29 -0300

    Menu interativo pronto, proximo passo: Responsividade

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ menuButton.addEventListener('click', () => {
 
 document.querySelectorAll('.toggleItem').forEach(n => n.addEventListener('click', () => {
     console.log('Retornando');
-    toggleMenu.classList.remove('active');
-
+    toggleMenu.classList.remove('active')
 }))
