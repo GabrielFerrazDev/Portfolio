b2dad061df9e40ee2e0a4e608843a4e3edafa37f
Gabriel Ferraz <gabrielferraz97@outlook.com>
2023-08-08 18:08:24 -0300

    overhaul

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const toggleMenu = document.getElementById('toggleMenu');
-const menuButton = document.getElementById('menuButton');
-
-
-menuButton.addEventListener('click', () => {
-    console.log('Funcionando');
-    toggleMenu.classList.toggle('active');
-})
-
-document.querySelectorAll('.toggleItem').forEach(n => n.addEventListener('click', () => {
-    console.log('Retornando');
-    toggleMenu.classList.remove('active')
-}))
+console.log(`Hello world!`);
